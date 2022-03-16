cb4596845aedd1aee255f041a90270b9e209f4ac
ester <95254487+est2r@users.noreply.github.com>
2022-03-16 18:57:50 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Exercicio-1/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  #include <stdio.h>
+int n;
+printf ("Digite um número inteiro:");
+scanf ("%d", &n);
+if (n>=0)
+  printf("Número Positivo");
+  else 
+  printf("Número Negativo");
+}
